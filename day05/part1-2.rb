cd628e44c5afa30fac057641bef75bf08cd530ce
Doug Nelson <doug@dochne.com>
2022-12-05 11:00:45 +0000

    added column substr solution

---
+++ day05/part1-2.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+data, instructions = STDIN.read.split("\n\n")
+
+cargo = data.split("\n")
+    .reduce([]) do |acc, line|
+        line.chars.each_with_index {|char, index| acc[index] = (acc[index] || "") + char}
+        acc
+    end
+    .map(&:strip)
+    .filter{|line| line.match(/^[A-Z]+\d$/)}
+
+instructions.scan(/move (\d+) from (\d+) to (\d+)/)
+    .map{|v| v.map(&:to_i)}
+    .each {|total, src, dest| cargo[dest-1] = cargo[src-1].slice!(0, total).reverse + cargo[dest-1] }
+
+value = cargo.map{|v| v[0]}.join("")
+p(value)
