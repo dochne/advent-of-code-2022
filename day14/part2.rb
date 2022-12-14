5ddf4a0fdbb27ec5d0a7fbd192bf671d3b2c5577
Doug Nelson <doug@dochne.com>
2022-12-14 08:29:28 +0000

    doug regrets not building a grid primitive

---
+++ day14/part2.rb
@@ -0,0 +1,71 @@
+#!/usr/bin/env ruby
+
+
+grid = {"500,0" => "+"}
+
+value = STDIN.read.lines(chomp: true)
+    .each do |line|
+        points = line
+            .split("->")
+            .map(&:strip)
+            .map{|v| v.split(",").map(&:to_i)}
+            .each_cons(2) do | points |
+                Range.new(points.map(&:first).min, points.map(&:first).max).each do |col|
+                    Range.new(points.map(&:last).min, points.map(&:last).max).each do |row|
+                        grid["#{col},#{row}"] = "#"
+                    end
+                end
+            end
+    end
+
+
+def draw(grid)    
+    walls = grid.keys.map{_1.split(",").map(&:to_i)}
+    left = walls.map(&:first).min
+    right = walls.map(&:first).max
+    floor = walls.map(&:last).max
+
+    Range.new(0, floor).each do |row|
+        print "#{row} "
+        Range.new(left, right).each do |col|
+            print(grid["#{col},#{row}"] || ".")
+        end
+        print("\n")
+    end
+end
+
+floor = walls = grid.keys.map{_1.split(",")}.map(&:last).map(&:to_i).max + 2
+
+def coord(col, row)
+    "#{col},#{row}"
+end
+
+while true do
+    # drop a piece of sand!
+    col = 500
+    row = 0
+    
+    while true do
+        if row + 1 == floor
+            grid[coord(col, row)] = "O"
+            break
+        elsif grid[coord(col, row + 1)].nil?
+            row += 1     
+        elsif grid[coord(col - 1, row + 1)].nil?
+            col -= 1
+            row += 1
+        elsif grid[coord(col + 1, row + 1)].nil?
+            col += 1
+            row += 1
+        elsif col == 500 && row == 0
+            draw(grid)
+            p(grid.filter{|_, v| v == "O"}.size + 1)
+            exit
+        else
+            grid[coord(col, row)] = "O"
+            break
+        end
+    end
+end
+
+draw(grid)
